10e6ba01e746da6e4b574723110c6858a8adec8c
Sailia <syema.ailia@gmail.com>
2016-06-07 19:29:36 -0500

    delete subject

--- simple_cms/app/controllers/subjects_controller.rb
+++ simple_cms/app/controllers/subjects_controller.rb
@@ -20,6 +20,7 @@ class SubjectsController < ApplicationController
     # * Save the object
     if @subject.save
     # * If save succeeds, redirect to to the index action
+    flash[:notice] = "Subject created successfully"
     redirect_to(:action => 'index')
     # * If save fails, redisplay the form so the user can fix problems
     else 
@@ -31,14 +32,15 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
   end
 
-    def update
+  def update
     # * Find an existing object using form parameters
     @subject = Subject.find(params[:id])
     # * Update the object
     if @subject.update_attributes(subject_params)
-    # * If save succeeds, redirect to to the index action
+    # * If update succeeds, redirect to to the index action
+    flash[:notice] = "Subject updated successfully"
     redirect_to(:action => 'show', :id => @subject.id)
-    # * If save fails, redisplay the form so the user can fix problems
+    # * If update fails, redisplay the form so the user can fix problems
     else 
       render('edit')
     end
@@ -50,6 +52,7 @@ class SubjectsController < ApplicationController
 
   def destroy
     subject = Subject.find(params[:id]).destroy
+    flash[:notice] = "Subject #{subject.name} destroyed successfully"
     redirect_to(:action => 'index')
   end
 
