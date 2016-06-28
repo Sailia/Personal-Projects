de11769d50d82bea34946c8d0530dcdae5bb4a5c
Sailia <syema.ailia@gmail.com>
2016-06-27 22:46:46 -0500

    date and time helpers

--- simple_cms/app/controllers/subjects_controller.rb
+++ simple_cms/app/controllers/subjects_controller.rb
@@ -66,7 +66,7 @@ class SubjectsController < ApplicationController
     # same as using "params[:subject]", except that it:
     # - raises an error if :subject isn't present
     # - allows listed attributes to be mass-assigned  
-    params.require(:subject).permit(:name, :position, :visible)
+    params.require(:subject).permit(:name, :position, :visible, :created_at)
   end
 
 end
